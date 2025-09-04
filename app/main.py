fa125cd7880feaf4921ad1e95ae18dca872d06f3
Aeneas Tews <aeneas.tews@icloud.com>
2025-09-04 17:21:35 +0200

    fixed re-search after every page update due to missing check of search field value

--- app/main.py
+++ app/main.py
@@ -54,6 +54,8 @@ if "construct_list_formatted" not in state:
     state.construct_list_formatted = {}
 if "download_data" not in state:
     state.download_data = None
+if "prev_search" not in state:
+    state.prev_search = ""
 
 def update_chain_highlight_selection(chain_id_to_toggle, current_pdb_selection):
     # Access the actual state of the specific checkbox that was changed
@@ -168,10 +170,16 @@ with col2:
     search_button = st.button("", key="search_button", icon=":material/search:", width=45)
 
 # search database and display options
+if search_field and state.prev_search != search_field:
+    with st.spinner(f"Searching for: **{search_field}**"):
+        state.sabdab, state.skempi, state.pdbs = search_antibodies(search_field)
+        state.prev_search = search_field
+
 if search_button:
     if search_field:
         with st.spinner(f"Searching for: **{search_field}**"):
             state.sabdab, state.skempi, state.pdbs = search_antibodies(search_field)
+            state.prev_search = search_field
     else:
         st.error("Please enter a search query.")
 
