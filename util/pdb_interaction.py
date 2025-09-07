753a56db457ef548a0d3935236616582b9cf7b15
Aeneas Tews <aeneas.tews@icloud.com>
2025-09-07 15:59:39 +0200

    removed necessity for large download - using rcsb api to download pdb files on the fly - changed setup file to not download structures - removed option to download "all pdbs" - combined different search options and if statements into single block - changed some syntax to be in line with rest

--- util/pdb_interaction.py
+++ util/pdb_interaction.py
@@ -1,11 +1,12 @@
 from Bio import SeqIO
 from io import StringIO
 import re
+import requests
 
-def extract_chains_from_pdb(file_path: str) -> list[dict]:
+def extract_chains_from_pdb(file_path: str | None = None, file_content: str | None = None) -> list[dict]:
     chains_data = []
     try:
-        records = SeqIO.parse(file_path, "pdb-atom")
+        records = SeqIO.parse(StringIO(file_content) if file_content else file_path, "pdb-atom")
         for record in records:
             if ":" in record.id:
                 chain_id_display = record.id.split(":")[-1]
@@ -29,6 +30,22 @@ def extract_chains_from_pdb(file_path: str) -> list[dict]:
     return chains_data
 
 
+def extract_chains_from_fasta(fasta: str) -> list[dict] | None:
+    chains_data = []
+    try:
+        fasta_io = StringIO(fasta)
+        records = SeqIO.parse(fasta_io, "fasta")
+
+        for record in records:
+            chains_data.append(record)
+
+        return chains_data
+
+    except Exception as e:
+        print(e)
+        return None
+
+
 def convert_chains_to_fasta_string(chains_data: list[dict]) -> str:
     """Converts a list of chain dictionaries to a FASTA formatted string."""
     fasta_string = StringIO()
@@ -44,5 +61,29 @@ def extract_fasta_from_pdb(file_path: str) -> str:
     chains_data = extract_chains_from_pdb(file_path)
     return convert_chains_to_fasta_string(chains_data)
 
+
 def cut_protein(file_content: str) -> str:
     raise Exception("Feature currently under construction!")
+
+
+def get_pdb_from_rcsb(pdb_id: str) -> str | None:
+    url = f"https://files.rcsb.org/download/{pdb_id}.pdb"
+
+    try:
+        res = requests.get(url)
+        res.raise_for_status()
+        return res.text
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching PDB file for {pdb_id}: {e}")
+        return None
+
+
+def get_fasta_from_rcsb(pdb_id: str) -> str | None:
+    url = f"https://www.rcsb.org/fasta/entry/{pdb_id}"
+    try:
+        res = requests.get(url)
+        res.raise_for_status()
+        return res.text
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching FASTA file for {pdb_id}: {e}")
+        return None
